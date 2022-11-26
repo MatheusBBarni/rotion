b00fa0853c8fb015840ddfff0d80378c310d6802
MatheusBBarni <brehm.matheus@hotmail.com>
2022-11-26 14:04:34 -0300

    feat: added tiptap as editor

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,18 +1,6 @@
-import { Header } from './components/Header'
-import { Sidebar } from './components/Sidebar'
+import { Routes } from './routes'
 import './styles/global.css'
 
 export function App() {
-  return (
-    <div className="h-screen w-screen bg-rotion-900 text-rotion-100 flex">
-      <Sidebar />
-      <div className="flex-1 flex flex-col max-h-screen">
-        <Header />
-
-        <main className="flex-1 flex items-center justify-center text-rotion-400">
-          Selecione ou crie um documento
-        </main>
-      </div>
-    </div>
-  )
+  return <Routes />
 }
---
+++ src/renderer/src/components/Editor/index.tsx
@@ -0,0 +1,35 @@
+import Document from '@tiptap/extension-document'
+import HighLight from '@tiptap/extension-highlight'
+import Placeholder from '@tiptap/extension-placeholder'
+import Typography from '@tiptap/extension-typography'
+import { EditorContent, useEditor } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+
+export function Editor() {
+  const editor = useEditor({
+    extensions: [
+      Document.extend({
+        content: 'heading block*',
+      }),
+      StarterKit.configure({
+        document: false,
+      }),
+      HighLight,
+      Typography,
+      Placeholder.configure({
+        placeholder: 'Untitled',
+        emptyEditorClass:
+          'before:content-[attr(data-placeholder)] before:text-gray-500 before:h-0 before:float-left before:pointer-events-none',
+      }),
+    ],
+    content: '<h1>Back-end</h1><p>Documento sobre back-end</p>',
+    autofocus: 'end',
+    editorProps: {
+      attributes: {
+        class: 'focus:outline-none prose prose-invert prose-headings:mt-0',
+      },
+    },
+  })
+
+  return <EditorContent editor={editor} className="w-[65ch]" />
+}
