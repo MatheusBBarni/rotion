276187fa84433e5314d84951ac3c87d7bdd228f5
MatheusBBarni <brehm.matheus@hotmail.com>
2022-11-26 12:11:59 -0300

    feat: added icons and created the app skeleton

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,16 +1,20 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
-import { resolve } from 'path'
+import { resolve } from 'node:path'
 import tailwindcss from 'tailwindcss'
 
 export default defineConfig({
   main: {
     plugins: [externalizeDepsPlugin()],
+    publicDir: resolve('resources'),
   },
   preload: {
     plugins: [externalizeDepsPlugin()],
   },
   renderer: {
+    define: {
+      'process.platform': JSON.stringify(process.platform),
+    },
     css: {
       postcss: {
         plugins: [
--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,9 +1,6 @@
-// prettier-ignore
 import { ElectronAPI } from '@electron-toolkit/preload'
 
-// prettier-ignore
 declare global {
-  // prettier-ignore
   export interface Window {
     electron: ElectronAPI
     api: unknown
