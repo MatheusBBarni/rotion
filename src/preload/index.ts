3587736c82169c2c89805873c8a914f893710cae
matheusBBarni <brehm.matheus@hotmail.com>
2022-12-09 23:42:05 -0300

    feat: added CRUD calls on preload

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,6 +1,13 @@
 import { ElectronAPI, electronAPI } from '@electron-toolkit/preload'
 import { IPC } from '@shared/constants/ipc'
-import { FetchAllDocumentsResponse } from '@shared/types/ipc'
+import {
+  CreateDocumentResponse,
+  DeleteDocumentRequest,
+  FetchAllDocumentsResponse,
+  FetchDocumentRequest,
+  FetchDocumentResponse,
+  SaveDocumentRequest,
+} from '@shared/types/ipc'
 import { contextBridge, ipcRenderer } from 'electron'
 
 declare global {
@@ -13,6 +20,18 @@ declare global {
 const api = {
   fetchDocuments(): Promise<FetchAllDocumentsResponse> {
     return ipcRenderer.invoke(IPC.DOCUMENTS.FETCH_ALL)
+  },
+  fetchDocument(req: FetchDocumentRequest): Promise<FetchDocumentResponse> {
+    return ipcRenderer.invoke(IPC.DOCUMENTS.FETCH, req)
+  },
+  createDocument(): Promise<CreateDocumentResponse> {
+    return ipcRenderer.invoke(IPC.DOCUMENTS.CREATE)
+  },
+  saveDocument(req: SaveDocumentRequest): Promise<void> {
+    return ipcRenderer.invoke(IPC.DOCUMENTS.SAVE, req)
+  },
+  deleteDocument(req: DeleteDocumentRequest): Promise<void> {
+    return ipcRenderer.invoke(IPC.DOCUMENTS.DELETE, req)
   },
 }
 
--- src/shared/types/ipc.ts
+++ src/shared/types/ipc.ts
@@ -5,8 +5,25 @@ export interface Document {
 }
 
 // Request
+export interface FetchDocumentRequest {
+  id: string
+}
+
+export interface DeleteDocumentRequest {
+  id: string
+}
+
+export type SaveDocumentRequest = Document
 
 // Response
 export interface FetchAllDocumentsResponse {
   data: Array<Document>
+}
+
+export interface FetchDocumentResponse {
+  data: Document
+}
+
+export  interface CreateDocumentResponse {
+  data: Document
 }
